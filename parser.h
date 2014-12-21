487693ba9877a5d5f51dc118224c386329ecb836
vinz243 <vinz243>
2014-12-21 17:22:27 +0100

    Add basic interface

---
+++ parser.h
@@ -0,0 +1,37 @@
+#include <string>
+#include <vector>
+
+namespace Parser {
+	struct Vector3
+	{
+		float x;
+		float y;
+		float z;
+	};
+	struct Entity
+	{
+		Vector3 		position;
+		Vector3			dimensions;
+		std::string 	id;
+		std::string 	mesh;
+	};
+	struct World
+	{
+		std::string 			name;
+		std::string 			version;
+		std::vector<Entity> 	entities;
+	};
+
+	/**
+	* \brief read a map file
+	* \param mapFile the absolute or relative path to the map-file.map
+	*/
+	void input(std::string mapFile);
+
+	/**
+	* \brief start building up the world
+	*/
+	World load();
+
+
+}
